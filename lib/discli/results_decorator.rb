352d8c2cf487a2282463beee4e10da04a4c821b6
Chris Hunt <c@squareup.com>
2012-07-29 12:45:11 -0700

    Add some output formatting to ResultsDecorator

--- lib/discli/results_decorator.rb
+++ lib/discli/results_decorator.rb
@@ -20,5 +20,37 @@ module Discli
         end
       end
     end
+
+    def to_s
+      results.map do |result|
+        "%s | %s | %s" % [
+          result['id'].to_s.rjust(9),
+          color_type(result['type']).ljust(16),
+          result['title']]
+      end.join("\n")
+    end
+
+    private
+
+    def color_type(type)
+      color = {
+        'release' => :red,
+        'master'  => :green,
+        'artist'  => :blue,
+        'label'   => :yellow
+      }.fetch(type.strip, :black)
+
+      send(color, type)
+    end
+
+    def red(text);    colorize(text, 32); end
+    def blue(text);   colorize(text, 34); end
+    def green(text);  colorize(text, 31); end
+    def black(text);  colorize(text, 00); end
+    def yellow(text); colorize(text, 33); end
+
+    def colorize(text, code)
+      "\e[#{code}m#{text}\e[0m"
+    end
   end
 end
